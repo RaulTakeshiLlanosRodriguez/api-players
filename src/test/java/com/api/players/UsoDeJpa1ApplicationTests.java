d51f743213ba4c176bc2fda70111ba4a237d7cf0
RaulTakeshiLlanosRodriguez <68862116+RaulTakeshiLlanosRodriguez@users.noreply.github>
2023-11-03 18:07:59 -0500

    config test class

--- src/test/java/com/api/players/UsoDeJpa1ApplicationTests.java
+++ src/test/java/com/api/players/UsoDeJpa1ApplicationTests.java
@@ -3,7 +3,7 @@ package com.api.players;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UsoDeJpa1ApplicationTests.class)
 class UsoDeJpa1ApplicationTests {
 
 	@Test
